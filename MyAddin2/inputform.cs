6c53d66f49617fcd12e6925d508b5bf5b57a70b9
FArpad <tyrael097@freemail.hu>
2013-09-19 04:03:33 +0200

    Week 2

---
+++ MyAddin2/inputform.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyAddin2
+{
+    public partial class inputform : Form
+    {
+        public string outref = "";
+
+        public inputform()
+        {
+            InitializeComponent();
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            outref = textBox1.Text;
+        }
+    }
+}
